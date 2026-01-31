fb735c5553241ebb4a5da1d7d2a7ddc6d44dc340
Dibyajyoti <dibyajyotipramanick0750@gmail.com>
2026-01-31 22:15:50 +0530

    send dashboard data

--- backend/controllers/educatorController.js
+++ backend/controllers/educatorController.js
@@ -1,6 +1,8 @@
 import { clerkClient } from "@clerk/express";
 import { v2 as cloudinary } from "cloudinary";
 import Course from "../models/Course.js";
+import Purchase from "../models/Purchase.js";
+import User from "../models/User.js";
 
 // Update role to educator
 export const updateRoleToEducator = async (req, res) => {
@@ -40,4 +42,66 @@ export const getEducatorCourses = async (req, res) => {
   const educator = await req.auth().userId;
   const courses = await Course.find({ educator });
   res.json({ success: true, courses });
+};
+
+// Get dashboard data
+export const educatorDashboardData = async (req, res) => {
+  const educator = await req.auth().userId();
+  const courses = await Course.find({ educator });
+  const totalCourses = courses.length;
+
+  const courseIds = courses.map((course) => course._id);
+
+  // calculate total earnings
+  const purchases = await Purchase.find({
+    courseId: { $in: courseIds },
+    status: "Completed",
+  });
+
+  const totalEarnings = purchases.reduce(
+    (sum, purchase) => sum + purchase.amount,
+    0,
+  );
+
+  // collect unique enrolled student IDs with their course titles
+  const enrolledStudentsData = [];
+  for (const course of courses) {
+    const students = await User.find(
+      {
+        _id: { $in: course.enrolledStudents },
+      },
+      "name imageUrl",
+    );
+
+    students.forEach((student) => {
+      enrolledStudentsData.push({ courseTitle: course.courseTitle, student });
+    });
+  }
+
+  res.json({
+    success: true,
+    dashboardData: { totalCourses, enrolledStudentsData, totalEarnings },
+  });
+};
+
+// Get enrolled students data with purchase data
+export const getEnrolledStudentsData = async (req, res) => {
+  const educator = await req.auth().userId;
+  const courses = await Course.find({ educator });
+  const courseIds = courses.map((course) => course._id);
+
+  const purchases = await Purchase.find({
+    courseId: { $in: courseIds },
+    status: "Completed",
+  })
+    .populate("userId", "name imageUrl")
+    .populate("courseId", "courseTitle");
+
+  const enrolledStudents = purchases.map((purchase) => ({
+    student: purchase.userId,
+    courseTitle: purchase.courseId.courseTitle,
+    purchaseDate: purchase.createdAt,
+  }));
+
+  res.json({ success: true, enrolledStudents });
 };
--- backend/routes/educatorRoutes.js
+++ backend/routes/educatorRoutes.js
@@ -2,7 +2,9 @@ import express from "express";
 import wrapAsync from "../middlewares/wrapAsync.js";
 import {
   addCourse,
+  educatorDashboardData,
   getEducatorCourses,
+  getEnrolledStudentsData,
   updateRoleToEducator,
 } from "../controllers/educatorController.js";
 import { protect, protectEducator } from "../middlewares/auth.js";
@@ -11,12 +13,26 @@ import upload from "../configs/multer.js";
 const educatorRouter = express.Router();
 
 educatorRouter.get("/update-role", protect, wrapAsync(updateRoleToEducator));
+
 educatorRouter.post(
   "/add-course",
   upload.single("image"),
   protectEducator,
-  addCourse,
+  wrapAsync(addCourse),
 );
-educatorRouter.get("/courses", protectEducator, getEducatorCourses);
+
+educatorRouter.get("/courses", protectEducator, wrapAsync(getEducatorCourses));
+
+educatorRouter.get(
+  "/dashboard",
+  protectEducator,
+  wrapAsync(educatorDashboardData),
+);
+
+educatorRouter.get(
+  "/enrolled-students",
+  protectEducator,
+  wrapAsync(getEnrolledStudentsData),
+);
 
 export default educatorRouter;
