2ac46931cda51cd8292f4c4a9681e80ac6938567
Dibyajyoti <dibyajyotipramanick0750@gmail.com>
2026-02-02 16:00:33 +0530

    course progress & ratings

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,7 @@ import Course from "../models/Course.js";
 import Purchase from "../models/Purchase.js";
 import User from "../models/User.js";
 import ExpressError from "../utils/expressError.js";
+import CourseProgress from "../models/CourseProgress.js";
 
 // Get user data
 export const getUserData = async (req, res) => {
@@ -76,4 +77,73 @@ export const purchaseCourse = async (req, res) => {
   });
 
   res.json({ success: true, session_url: session.url });
+};
+
+// Update user course progress
+export const updateUserCourseProgress = async (req, res) => {
+  const { userId } = await req.auth();
+  const { courseId, lectureId } = req.body;
+  const progressData = await CourseProgress.findOne({ userId, courseId });
+
+  if (progressData) {
+    if (progressData.lectureCompleted.includes(lectureId)) {
+      throw new ExpressError(409, "Lecture already completed"); // The resource exists, but the operation conflicts with its current state.
+    }
+
+    progressData.lectureCompleted.push(lectureId);
+    await progressData.save();
+  } else {
+    await CourseProgress.create({
+      userId,
+      courseId,
+      lectureCompleted: [lectureId],
+    });
+  }
+
+  res.json({ success: true, message: "Progress updated" });
+};
+
+// Get user course progress
+export const getUserCourseProgress = async (req, res) => {
+  const { userId } = await req.auth();
+  const { courseId } = req.body;
+  const progressData = await CourseProgress.findOne({ userId, courseId });
+  res.json({ success: true, progressData });
+};
+
+// Add user ratings to course
+export const addUserRating = async (req, res) => {
+  const { userId } = await req.auth();
+  const { courseId, rating } = req.body;
+
+  if (!courseId || !userId || !(rating >= 1 && rating <= 5)) {
+    throw new ExpressError(400, "Invalid details");
+  }
+
+  const course = await Course.findById(courseId);
+
+  if (!course) {
+    throw new ExpressError(404, "Course not found");
+  }
+
+  const user = await User.findById(userId);
+
+  if (!user || !user.enrolledCourses.includes(courseId)) {
+    throw new ExpressError(403, "User has not purchased this course");
+  }
+
+  const existingRatingIndex = course.courseRatings.findIndex(
+    // findIndex returns: index 0, 1, 2, ... if found -1 if not found
+    (r) => r.userId === userId,
+  );
+
+  if (existingRatingIndex > -1) {
+    course.courseRatings[existingRatingIndex].rating = rating;
+  } else {
+    course.courseRatings.push({ userId, rating });
+  }
+
+  await course.save();
+
+  res.json({ success: true, message: "Rating added" });
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,8 +2,11 @@ import express from "express";
 import { protect } from "../middlewares/auth.js";
 import wrapAsync from "../middlewares/wrapAsync.js";
 import {
+  addUserRating,
+  getUserCourseProgress,
   getUserData,
   purchaseCourse,
+  updateUserCourseProgress,
   userEnrolledCourses,
 } from "../controllers/userController.js";
 
@@ -12,5 +15,9 @@ const userRouter = express.Router();
 userRouter.get("/data", protect, wrapAsync(getUserData));
 userRouter.get("/enrolled-courses", protect, wrapAsync(userEnrolledCourses));
 userRouter.post("/purchase", protect, wrapAsync(purchaseCourse));
+
+userRouter.get("/course-progress", protect, getUserCourseProgress);
+userRouter.post("/update-course-progress", protect, updateUserCourseProgress);
+userRouter.post("/add-rating", protect, addUserRating);
 
 export default userRouter;
