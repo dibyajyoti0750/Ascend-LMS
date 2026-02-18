84ca2899a9c6c074ef55cdd1f86c16948c7bad8c
Dibyajyoti <dibyajyotipramanick0750@gmail.com>
2026-02-18 11:17:44 +0530

    style fixes

--- frontend/src/components/educator/EditCourseModal.tsx
+++ frontend/src/components/educator/EditCourseModal.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import type { EditCourse } from "../../features/educator/data.types";
 import { UploadCloud, X } from "lucide-react";
+import Quill from "quill";
 
 interface Props {
   course: EditCourse;
@@ -9,16 +10,15 @@ interface Props {
 }
 
 export default function EditCourseModal({ course, onClose, onSave }: Props) {
+  const quillRef = useRef<Quill | null>(null);
+  const descRef = useRef<HTMLDivElement | null>(null);
+
   const [formData, setFormData] = useState({
-    courseTitle: course.courseTitle,
-    courseDescription: course.courseDescription,
-    coursePrice: course.coursePrice,
-    discount: course.discount,
-    isPublished: course.isPublished,
+    ...course,
   });
 
   const [thumbnailPreview, setThumbnailPreview] = useState(
-    course.courseThumbnail,
+    formData.courseThumbnail,
   );
 
   const [thumbnailFile, setThumbnailFile] = useState<File | undefined>();
@@ -29,11 +29,25 @@ export default function EditCourseModal({ course, onClose, onSave }: Props) {
 
   const handleThumbnailChange = (e: React.ChangeEvent<HTMLInputElement>) => {};
 
+  useEffect(() => {
+    if (!quillRef.current && descRef.current) {
+      quillRef.current = new Quill(descRef.current, {
+        theme: "snow",
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (quillRef.current && formData.courseDescription) {
+      quillRef.current.root.innerHTML = formData.courseDescription;
+    }
+  }, [formData.courseDescription]);
+
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/60 backdrop-blur-sm p-4">
-      <div className="w-full max-w-2xl bg-white rounded-xl shadow-2xl border border-gray-100 overflow-hidden">
+      <div className="w-full max-w-2xl bg-white rounded-xl shadow-2xl border border-gray-100 flex flex-col max-h-[90vh]">
         {/* Header */}
-        <div className="flex items-center justify-between px-6 py-4 border-b border-gray-200">
+        <div className="flex items-center justify-between px-6 py-4 border-b border-gray-300 shrink-0">
           <h2 className="text-lg font-semibold text-gray-800">Edit Course</h2>
 
           <button
@@ -45,7 +59,10 @@ export default function EditCourseModal({ course, onClose, onSave }: Props) {
         </div>
 
         {/* Form */}
-        <form onSubmit={onSave} className="px-6 py-6 space-y-6">
+        <form
+          onSubmit={onSave}
+          className="flex-1 overflow-y-auto px-5 py-3 space-y-4"
+        >
           {/* Title */}
           <div className="space-y-2">
             <label className="text-sm font-medium text-gray-700">
@@ -62,23 +79,16 @@ export default function EditCourseModal({ course, onClose, onSave }: Props) {
           </div>
 
           {/* Description */}
-          <div className="space-y-2">
-            <label className="text-sm font-medium text-gray-700">
+          <div className="space-y-2 text-sm">
+            <label className="font-medium text-gray-700">
               Course Description
             </label>
-            <textarea
-              name="courseDescription"
-              value={formData.courseDescription}
-              onChange={handleChange}
-              rows={4}
-              className="w-full rounded-lg border border-gray-300 bg-gray-50 px-4 py-2.5 text-sm focus:outline-none focus:ring-2 focus:ring-black focus:border-black transition resize-none"
-              required
-            />
+            <div ref={descRef}></div>
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
             {/* Left Side */}
-            <div className="md:col-span-2 space-y-5">
+            <div className="md:col-span-2 space-y-6">
               <div className="space-y-2">
                 <label className="text-sm font-medium text-gray-700">
                   Price
@@ -110,7 +120,7 @@ export default function EditCourseModal({ course, onClose, onSave }: Props) {
                 />
               </div>
 
-              <div className="flex items-center gap-3 pt-2">
+              <div className="flex items-center gap-2">
                 <input
                   type="checkbox"
                   name="isPublished"
@@ -159,25 +169,26 @@ export default function EditCourseModal({ course, onClose, onSave }: Props) {
               </label>
             </div>
           </div>
-
-          {/* Buttons */}
-          <div className="flex justify-end gap-3 pt-4 border-t border-gray-200">
-            <button
-              type="button"
-              onClick={onClose}
-              className="px-5 py-2.5 text-sm font-medium rounded-lg border border-gray-300 text-gray-700 hover:bg-gray-100 transition cursor-pointer"
-            >
-              Cancel
-            </button>
-
-            <button
-              type="submit"
-              className="px-5 py-2.5 text-sm font-medium rounded-lg bg-purple-800 text-white hover:bg-purple-900 transition shadow-md cursor-pointer"
-            >
-              Save Changes
-            </button>
-          </div>
         </form>
+
+        {/* Buttons */}
+        <div className="flex justify-end gap-3 px-6 py-4 border-t border-gray-300 shrink-0">
+          <button
+            type="button"
+            onClick={onClose}
+            className="px-5 py-2.5 text-sm font-medium rounded-lg border border-gray-300 text-gray-700 hover:bg-gray-100 transition cursor-pointer"
+          >
+            Cancel
+          </button>
+
+          <button
+            type="submit"
+            onClick={onSave}
+            className="px-5 py-2.5 text-sm font-medium rounded-lg bg-purple-800 text-white hover:bg-purple-900 transition shadow-md cursor-pointer"
+          >
+            Save Changes
+          </button>
+        </div>
       </div>
     </div>
   );
