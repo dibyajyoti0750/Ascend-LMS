4cbc393fe044ff8f6532fe71bc9349a5af4c0b82
Dibyajyoti <dibyajyotipramanick0750@gmail.com>
2026-03-03 16:30:45 +0530

    hero header

--- frontend/src/components/student/Hero.tsx
+++ frontend/src/components/student/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
   return (
     <div className="flex flex-col items-center justify-center w-full pt-16 px-8 md:px-0 space-y-10 text-center bg-linear-to-b from-purple-100/80">
       <h1 className="text-4xl md:text-5xl font-black text-gray-800 max-w-3xl mx-auto text-center">
-        Master High-Income Skills &{" "}
+        Master Valuable Skills &{" "}
         <span className="text-[#6F00FF]">
           <Typewriter
             words={["Build Real Results"]}
